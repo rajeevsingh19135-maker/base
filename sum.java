89b5d69ff56e79f8a5c3d6c864367228128aca6e
Rajeev Singh <rajeevsingh19135@gmail.com>
2025-11-23 16:36:10 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+package base;
+
+public class sum {
+    public static void main(String[] args) {
+        int a=10,b=30;
+        System.out.println(a+b);
+        System.out.println(a-b);
+    }
+}
