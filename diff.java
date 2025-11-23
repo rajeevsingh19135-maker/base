f862236905a2a0287dea565bd12bbeedcb389bc5
Rajeev Singh <rajeevsingh19135@gmail.com>
2025-11-23 17:20:57 +0530

    add new

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ package base;
 
 public class diff {
     public static void main(String[] args) {
-        
+        System.out.println("hello");
     }
 }
--- sum.java
+++ sum.java
@@ -5,5 +5,7 @@ public class sum {
         int a=10,b=30;
         System.out.println(a+b);
         System.out.println(a-b);
+        System.out.println(a-b);
+
     }
 }
